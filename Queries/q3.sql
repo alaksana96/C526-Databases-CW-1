14a866d8c429a94b5d794bf8b1cbb6460e779be3
Aufar Laksana <alaksana96@gmail.com>
2017-02-20 11:12:26 +0000

    Q3

---
+++ Queries/q3.sql
@@ -0,0 +1,9 @@
+SELECT	name
+FROM	person AS mothers
+WHERE NOT EXISTS ( SELECT  gender
+      	  	   FROM	   person
+		   EXCEPT  
+		   SELECT  gender
+		   FROM	   person
+		   WHERE   mothers.name = person.mother
+		 )
